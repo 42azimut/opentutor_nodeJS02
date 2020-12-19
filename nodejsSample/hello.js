6518c3b3dded651ed702ad9ebf8324422a6b3335
Jayden Jeong <azimut_mac@Jaydens-MacBook-Pro.local>
2020-12-20 06:14:02 +0900

    by update done, begin for delete

---
+++ nodejsSample/hello.js
@@ -0,0 +1,17 @@
+console.log("Hello Node JS");
+console.log(2**3);
+console.log(12 % 2);
+console.log(12 / 2);
+console.log("22" + "33");
+
+var name_my = 'Jayden';
+var array_lorem = `Dear ${name_my}
+
+Lorem  ${name_my} ${2+3} pariatur deserunt do consequat ad qui voluptate 
+commodo non qui esse laborum ex. 
+Culpa id ut cupidatat culpa. Aliquip enim duis ${name_my} tempor dolore aliquip eiusmod exercitation.
+Reprehenderit magna in commodo ut dolore nulla minim eu nisi ex do elit. ${name_my}`;
+
+console.log(array_lorem);
+console.log(array_lorem.length);
+
